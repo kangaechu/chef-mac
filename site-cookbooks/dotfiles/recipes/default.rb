ef309e36a82d1e918a5d26475703eb2e7d7f0f92
endok <keisuke.endo9@gmail.com>
2013-06-16 17:12:28 +0900

    dotfilesを追加。

---
+++ site-cookbooks/dotfiles/recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: dotfiles
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+git node["user"]["home"] + "/.dotfiles" do
+  user node["user"]["name"]
+  group node["user"]["group"]
+  repository node["user"]["dotfiles_repo"]
+  reference "master"
+  action :checkout
+  notifies :run, "bash[install dotfiles]"
+end
+
+bash "install dotfiles" do
+  user node["user"]["name"]
+  group node["user"]["group"]
+  cwd node["user"]["home"]
+  environment "HOME" => node["user"]["home"]
+  code <<-EOC
+    $HOME/.dotfiles/install.sh
+  EOC
+end
